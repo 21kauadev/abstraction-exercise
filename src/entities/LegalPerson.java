942bd92fbe3b43dd962de71933347f1a912e37d9
21kauadev <kauantnetsoares@gmail.com>
2025-02-17 12:26:59 -0300

    feat: add class LegalPerson with it tax calculation method

--- src/entities/LegalPerson.java
+++ src/entities/LegalPerson.java
@@ -2,4 +2,30 @@ package entities;
 
 public class LegalPerson extends Person {
 
+    private Integer numberOfEmployees;
+
+    public LegalPerson() {
+        super();
+    }
+
+    public LegalPerson(String name, Double annualIncome, Integer numberOfEmployees) {
+        super(name, annualIncome);
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public Integer getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    @Override
+    public Double tax() {
+        // verifica, se for verdadeiro, já retorna o valor e a função para.
+        if (numberOfEmployees > 10) {
+            return this.annualIncome * 0.14;
+        }
+
+        // caso contrário executa normalmente.
+
+        return this.annualIncome * 0.16;
+    }
 }
